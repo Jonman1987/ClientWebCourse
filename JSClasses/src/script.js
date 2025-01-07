5eb81fdad24833b924f2f10f85405f4cca7e2973
Jonman <Jonman@yandex.ru>
2025-01-07 23:33:57 +0700

    new commit 26

--- JSClasses/src/script.js
+++ JSClasses/src/script.js
@@ -41,3 +41,32 @@ function dogExample() {
     const dog = new Dog("Собака Чарли");
     dog.speak();
 } dogExample();
+
+class Animals1 {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} издает звук`);
+    }
+}
+
+class Cat1 extends Animals1 {
+    speak() {
+        console.log(`${this.name} мяукает`);
+    }
+}
+
+class Dog1 extends Animals1 {
+    speak() {
+        console.log(`${this.name} гавкает`);
+    }
+}
+
+const tiger = new Animals1("Тигр");
+const cat1 = new Cat1("Кошка Муся");
+const dog1 = new Dog1("Собака Чарли");
+tiger.speak();
+cat1.speak();
+dog1.speak();
